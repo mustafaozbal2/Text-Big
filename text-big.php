b0d6098d26a761c9124bfd134e3f2b3d7926126b
Mustafa Özbal <99390718+mustafaozbal2@users.noreply.github.com>
2023-01-18 12:25:39 +0300

    Add files via upload

---
+++ text-big.php
@@ -0,0 +1,6 @@
+                   <form action="yukle.php" method="post" enctype="multipart/form-data">
+    
+    <input type="file" name="dosya" />
+    <input type="submit" value="" />
+</form>
+         
